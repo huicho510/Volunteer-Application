d0326b0e5eef584f9e50c742f455ac882e952e56
Luis Cervantes <luiscerv1229@gmail.com>
2020-07-16 20:16:24 -0700

    added log/in/out switcharoo, added function to routes to validate login is true

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,7 +8,7 @@ module.exports = function(app) {
       res.redirect("/members");
     }
     res.render("index", {
-      login: userLoggedIn(req),
+      login: userLoggedIn(req)
     });
   });
   app.get("/login", (req, res) => {
@@ -17,7 +17,7 @@ module.exports = function(app) {
       res.redirect("/members");
     }
     res.render("login", {
-      login: userLoggedIn(req),
+      login: userLoggedIn(req)
     });
   });
   app.get("/signup", (req, res) => {
@@ -26,7 +26,7 @@ module.exports = function(app) {
       res.redirect("/members");
     }
     res.render("signup", {
-      login: userLoggedIn(req),
+      login: userLoggedIn(req)
     });
   });
 
@@ -34,7 +34,7 @@ module.exports = function(app) {
     // If the user already has an account send them to the members page
 
     res.render("about", {
-      login: userLoggedIn(req),
+      login: userLoggedIn(req)
     });
   });
 
@@ -42,7 +42,7 @@ module.exports = function(app) {
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, (req, res) => {
     res.render("members", {
-      login: userLoggedIn(req),
+      login: userLoggedIn(req)
     });
   });
 };
