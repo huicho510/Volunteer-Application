77d4207d723cd4d1d8f95e9a872067864c11f52e
Binh Nguyen <Binh.dai.nguyen.97@gmail.com>
2020-07-21 10:58:49 -0700

    added validate len

--- models/events.js
+++ models/events.js
@@ -1,36 +1,60 @@
 module.exports = function(sequelize, DataTypes) {
-  const Event = sequelize.define("events", {
+  const Event = sequelize.define("Event", {
     title: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        len: [1, 100]
+      }
     },
     address: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        len: [1, 50]
+      }
     },
     city: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        len: [1, 50]
+      }
     },
     state: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        len: [2]
+      }
     },
     zip: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        len: [5]
+      }
     },
     description: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        len: [1, 2000]
+      }
     },
     timeFrame: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        len: [1, 20]
+      }
     },
     postedDate: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        len: [1, 20]
+      }
     }
   });
   return Event;
